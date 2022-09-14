165c16df69340632b8db408f6ee57aac05c2d009
Simone Perera <simoperera@gmail.com>
2022-09-13 23:35:26 -0300

    Entrega DOM

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-function Entrada(fecha,titulo,texto){
+function Entrada(id,fecha,titulo,texto){
+    this.id=id
     this.fecha = fecha;
     this.titulo = titulo;
     this.texto = texto;
@@ -8,29 +9,36 @@ const Diario = [];
 let F1 ;
 let T1 ;
 let Tx1 ;
-let existe;
-let consulta = prompt("desea crear una entrada ? En caso afirmativo digite S, de lo contrario digite cualquier caracter");
-while (consulta.toUpperCase() == "S") { 
+let Id=1;
+let existe=false;
+while (confirm("Desea crear una nueva entrada ?")) { 
     F1 = prompt("Ingrese fecha de hoy");
     T1 = prompt("Ingrese el titulo de la entrada (Recuerde que este NO puede repetirse)");
-    existe = Diario.some( (E) => E.titulo = T1);
+    existe = Diario.some( (E) => E.titulo == T1);
     if (existe == true ){
         alert("ya existe una entrada con este titulo ");
     } else {
     Tx1 = prompt("Ingrese el texto correspondiente a la entrada");
-    const Entrada1 = new Entrada(F1,T1,Tx1);
+    const Entrada1 = new Entrada(Id,F1,T1,Tx1);
     Diario.push(Entrada1);
+    Id=Id +1;
     alert(Entrada1.caracteres + " caracteres ingresados");}
-    consulta = prompt("desea crear una nueva entrada ? En caso afirmativo digite S, de lo contrario cualquier caracter");
-}  
-    let consulta2 = prompt("Desea saber cuantas entradas fueron realizadas ? digite S en caso afirmativo, de lo contrario cualquier caracter ")
-    if (consulta2.toUpperCase() == "S"){
-        alert(Diario.length + " entradas fueron realizadas");
+    }  
+if (confirm("Desea saber cuantas entradas fueron realizadas ? ")){
+    alert(Diario.length + " entradas fueron realizadas");
+}
+if (confirm("Desea ver sus entradas en pantalla?")){
+    for (Entrada of Diario) {
+        let contenedor = document.createElement("div");
+        contenedor.innerHTML = `<h3>${Entrada.id}</h3>
+        <h4>${Entrada.titulo}</h4>
+        <p>${Entrada.texto}</p>
+        <p style="color:grey id = ${Entrada.id};">${Entrada.fecha}</p>
+        <hr>
+        `;
+        document.body.appendChild(contenedor);
     }
-    if (confirm("Desea er en la consola sus entradas ?")){
-        Diario.forEach( (Entrada) => {
-            console.log (Entrada.titulo)
-        });
-    }
-
-
+}
+if(confirm("Desea personalizar el titulo de su bitacora ?")){
+    const Bita = (document.querySelector("h1").textContent = prompt("Ingrese el nombre de su bitacora"));
+}
