c6818ded7e17e075a3ccfb9b6e096ea145ccf0e0
Simone Perera <simoperera@gmail.com>
2022-09-29 23:48:10 -0300

    Entrega Librerias

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ tml.innerHTML = `<input id = "titulo" class="form-control my-4" type="text" plac
 <input id = "fecha" class="form-control my-4" type="text" placeholder="Fecha"  >
 <input id = "Entr" class="form-control my-4 py-5" type="text" placeholder="Entrada"  >
 <button id= "Realizar" class="btn btn-primary ms-4 mb-3">Submit</button>
-<button id= "Imprimir"  class="btn btn-primary mb-3">imprimir</button> `;
+<button id= "Imprimir"  class="btn btn-primary mb-3">imprimir</button> 
+<button id= "EliminarTodo"  class="btn btn-primary mb-3">Eliminar Todo </button>`;
 sec.appendChild(tml);
 let tit2 = document.createElement("h2");
 tit2.innerHTML = `<h2 class="my-4" >Entradas:</h2>`;
@@ -29,28 +30,57 @@ entry.onclick= () => {
     F1 = document.querySelector("#fecha").value ;
     Tx1 = document.querySelector("#Entr").value ;
     const Entrada1 = new Entrada(F1,T1,Tx1);
-    const paraJSON = JSON.stringify(Entrada1);//  las entradas se imprimen a pesar de que 
+    const paraJSON = JSON.stringify(Entrada1);
     localStorage.setItem( T1, paraJSON ) ;
+    Toastify({
+        text: "Reaizaste una entrada !",
+        duration: 3000,
+        gravity: 'bottom',
+        position: 'left'
+    }).showToast();
 };
 let impres;
 let obj;
-let imp = document.querySelector("#Imprimir");  
-imp.addEventListener("click",imprimiendo());
-function imprimiendo(){
+const imp = document.querySelector("#Imprimir");   
+imp.addEventListener('click',() => {    
     for (let i=0; i < localStorage.length; i++) {
-        let clave = localStorage.key(i);
-        obj = JSON.parse(localStorage.getItem(clave));
-        impres =  document.createElement("div");
-        impres.innerHTML = `
-        <div class="card me-3 mb-2">
-            <div class="card-body">
-                <h3 class="card-title">${obj.titulo}</h3>
-                <p class="card-text">${obj.texto}</p>
-                <p class="card-text"><small class="text-muted">${obj.fecha}</small></p>
-            </div>
+    let clave = localStorage.key(i);
+    obj = JSON.parse(localStorage.getItem(clave));
+    impres =  document.createElement("div");
+    impres.innerHTML = `
+    <div class="card me-3 mb-2">
+        <div class="card-body">
+            <h3 class="card-title">${obj.titulo}</h3>
+            <p class="card-text">${obj.texto}</p>
+            <p class="card-text"><small class="text-muted">${obj.fecha}</small></p>
         </div>
-        `;
-        sec.appendChild(impres);
-
+    </div>
+    `;
+    document.body.appendChild(impres);
     }
-}
+    });
+const ET = document.querySelector("#EliminarTodo");
+ET.onclick= () => {     
+    Swal.fire({
+        title: 'Está seguro de eliminar sus entradas ?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, seguro',
+        cancelButtonText: 'No quiero'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Borrado!',
+                icon: 'success',
+                text: 'Sus entradas han sido borradas'
+            });
+            localStorage.clear();
+        }
+        else {
+            Swal.fire({
+                icon: 'success',
+                text: 'Sus entradas NO han sido eliminadas '
+            });
+        }
+    })
+} ;
