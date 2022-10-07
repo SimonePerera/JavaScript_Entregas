74ad22dffa7a1d25b7a30030e6a227d302da2c35
Simone Perera <simoperera@gmail.com>
2022-10-06 22:46:42 -0300

    Entrega Fetch

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function Entrada(fecha,titulo,texto){
-    this.fecha = fecha;
-    this.titulo = titulo;
-    this.texto = texto;
-    this.caracteres = texto.length ;
-}
+// function Entrada(fecha,titulo,texto){
+//     this.fecha = fecha;
+//     this.titulo = titulo;
+//     this.texto = texto;
+//     this.caracteres = texto.length ;
+// }
 let sec = document.querySelector("#sec");
 let tit = document.createElement("h1");
 tit.innerHTML = '<h1 id="encabezado" class = "text-center fw-bold">Bitacora</h1>';
@@ -26,12 +26,6 @@ let Tx1 ;
 let entry = document.querySelector("#Realizar");
 
 entry.onclick= () => {
-    T1 = document.querySelector("#titulo").value;
-    F1 = document.querySelector("#fecha").value ;
-    Tx1 = document.querySelector("#Entr").value ;
-    const Entrada1 = new Entrada(F1,T1,Tx1);
-    const paraJSON = JSON.stringify(Entrada1);
-    localStorage.setItem( T1, paraJSON ) ;
     Toastify({
         text: "Reaizaste una entrada !",
         duration: 3000,
@@ -40,47 +34,25 @@ entry.onclick= () => {
     }).showToast();
 };
 let impres;
-let obj;
+function Mostrar(){  
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((resp)=>resp.json())
+        .then((data)=>{
+            data.forEach((post)=>{
+                const impres = document.createElement("div")
+                impres.innerHTML=`
+                    <div class="card me-3 mb-2">
+                        <div class="card-body">
+                            <h3 class="card-title">${post.title}</h3>
+                            <p class="card-text">${post.body}</p>
+                            <p class="card-text"><small class="text-muted">${post.id}</small></p>
+                        </div>
+                    </div>
+                `
+                document.body.appendChild(impres)
+            })
+})
+
+}
 const imp = document.querySelector("#Imprimir");   
-imp.addEventListener('click',() => {    
-    for (let i=0; i < localStorage.length; i++) {
-    let clave = localStorage.key(i);
-    obj = JSON.parse(localStorage.getItem(clave));
-    impres =  document.createElement("div");
-    impres.innerHTML = `
-    <div class="card me-3 mb-2">
-        <div class="card-body">
-            <h3 class="card-title">${obj.titulo}</h3>
-            <p class="card-text">${obj.texto}</p>
-            <p class="card-text"><small class="text-muted">${obj.fecha}</small></p>
-        </div>
-    </div>
-    `;
-    document.body.appendChild(impres);
-    }
-    });
-const ET = document.querySelector("#EliminarTodo");
-ET.onclick= () => {     
-    Swal.fire({
-        title: 'Está seguro de eliminar sus entradas ?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Sí, seguro',
-        cancelButtonText: 'No quiero'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            Swal.fire({
-                title: 'Borrado!',
-                icon: 'success',
-                text: 'Sus entradas han sido borradas'
-            });
-            localStorage.clear();
-        }
-        else {
-            Swal.fire({
-                icon: 'success',
-                text: 'Sus entradas NO han sido eliminadas '
-            });
-        }
-    })
-} ;
+imp.addEventListener('click', Mostrar());
